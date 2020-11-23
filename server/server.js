9ac2dc4b13a7339440207dd56de41218d42226a9
Nate Foss <nathanielfoss11@gmail.com>
2020-11-23 15:03:33 -0700

    Proxy working for all services

--- server/server.js
+++ server/server.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'), err => {
+  res.sendFile(path.join(__dirname, '../client/index.html'), err => {
     if (err) {
       res.status(500).send(err);
     }
