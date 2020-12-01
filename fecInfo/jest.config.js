6de22efad362710a437f162df04247bfe8162a24
CCBaxter84 <troyceclark@gmail.com>
2020-12-01 14:06:24 -0700

    Dockerized and finalized with AWS instance public IPs

--- fecInfo/jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-// Sync object
-module.exports = {
-  verbose: true,
-  setupFiles: [
-    '<rootDir>/client/src/Tests.test.jsx',
-  ]
-};
--- fecReviews/client/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import ReviewApp from './App.jsx';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<ReviewApp/>, document.getElementById('ReviewApp'));
